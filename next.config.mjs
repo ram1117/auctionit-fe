dc097467c595dc52ec7ca6cfc9a8ddd4ed5ec7c0
ram1117 <ramkumark1@outlook.com>
2024-04-14 21:47:52 +0530

    fix eslint error in nextjs config

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: process.env.NEXT_PUBLIC_IMAGE_HOST,
+        hostname: 'zkapgrjeqxzbewuhtvdn.supabase.co',
       },
     ],
   },
