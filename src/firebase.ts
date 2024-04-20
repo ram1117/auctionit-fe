6db7c0796b6ca9ab5f814fa281bfa505652656c3
ram1117 <ramkumark1@outlook.com>
2024-04-21 00:39:21 +0530

    complete creating user oriented pages

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@
 import { initializeApp } from 'firebase/app'
 
 const FIREBASE_CONFIG = {
-  apiKey: 'AIzaSyAuSHX2HGtiAeKNW54lJBjMXf4R2YHnur0',
-  authDomain: 'auctionit-19fc4.firebaseapp.com',
-  projectId: 'auctionit-19fc4',
-  storageBucket: 'auctionit-19fc4.appspot.com',
-  messagingSenderId: '220656588174',
-  appId: '1:220656588174:web:dbddf04b0e49024abc00df',
-  measurementId: 'G-XWMMH6X1CG',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
 const firebaseApp = initializeApp(FIREBASE_CONFIG)
