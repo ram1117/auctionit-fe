d47a5269d232069c2bb57633d540bb081aff7d53
ram1117 <ramkumark1@outlook.com>
2024-04-23 17:57:36 +0530

    update readme

--- src/components/rightpanel/CategoriesContainer.tsx
+++ src/components/rightpanel/CategoriesContainer.tsx
@@ -12,12 +12,12 @@ const CategoriesContainer = ({
 }: CategoriesContainerProps) => {
   return (
     <section className="py-1 m-1">
-      <ul className="text-secondary-text font-semibold flex lg:grid lg:grid-cols-6 gap-1 lg:gap-4 p-1 lg:p-4 my-1 lg:my-4 text-sm lg:text-base">
+      <ul className="overflow-x-auto text-secondary-text font-semibold flex lg:grid lg:grid-cols-6 gap-1 lg:gap-4 p-1 lg:p-4 my-1 lg:my-4 text-sm lg:text-base">
         <button
-          className="capitalize tracking-tight text-sm gap-2 rounded-md py-1 lg:py-2 bg-white shadow-md shadow-slate-300"
+          className="capitalize tracking-tight text-sm gap-2 rounded-md py-1 lg:py-2 px-6 bg-white shadow-md shadow-slate-300"
           onClick={() => handleCategoryClick(0)}
         >
-          <h4 className="text-nowrap lg:text-wrap">All</h4>
+          All
         </button>
         {categories.map((category: CategoryType) => (
           <CategoryItem
